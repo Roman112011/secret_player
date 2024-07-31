1aafcdd76ddc9d120ee1d1bb37cf6f94be235cec
Роман Роман <romchikgoro@gmail.com>
2024-07-31 22:41:59 +0300

    modified file

---
+++ secret_player.js
@@ -0,0 +1,10 @@
+document.getElementById('log-in').onclick = function() {
+    let nickname = document.getElementById("nickname").value;
+    let password = document.getElementById("password").value;
+
+    if (nickname === "1" && password === "1") {
+
+        window.location.href="roman112011.github.io/secret_player/users/Secret.html";
+        alert("hell)");
+    }
+}
